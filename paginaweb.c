f1c91847275ba8572f537d35078f5d200593cf33
Diego Martín González <diego.martin.gonzalez@alumnos.upm.es>
2021-07-11 23:55:16 +0200

    switch en el inicio y cambio de nombre de variables

--- paginaweb.c
+++ paginaweb.c
@@ -5,10 +5,11 @@ void menu_inicio(void);
 void tiendas(void);
 int main()
 {
-    FILE *pInfo, *pDireccion_Hora, *pCharcuteria, *pFruteria, *pPanaderia, *pPescaderia;
-    char *DyH, *Info;
-    long int SizeFicheroDyH, SizeFicheroInfo;
+    FILE *pInformacion, *pDireccion_Hora, *pCharcuteria, *pFruteria, *pPanaderia, *pPescaderia;
+    char *DireccionyHorarios, *Informacion;
+    long int SizeFicheroDireccionyHorarios, SizeFicheroInformacion;
     int i;
+    char letratiendas_Acerca_Dir, letrainicio;
 
     //Fichero con la direccion y horario del mercado
 
@@ -21,48 +22,76 @@ int main()
     else
     {
         fseek(pDireccion_Hora, 0, SEEK_END);
-        SizeFicheroDyH=ftell(pDireccion_Hora);
+        SizeFicheroDireccionyHorarios=ftell(pDireccion_Hora);
         fseek(pDireccion_Hora, 0, SEEK_SET);
-        DyH=malloc(SizeFicheroDyH);
-        if(DyH==NULL)
+        DireccionyHorarios=malloc(SizeFicheroDireccionyHorarios);
+        if(DireccionyHorarios==NULL)
         {
             printf("Error: memoria insuficiente\n");
             exit(-1);
         }
         i=0;
-        while(fscanf(pDireccion_Hora, "%c", &DyH[i])!=EOF)
+        while(fscanf(pDireccion_Hora, "%c", &DireccionyHorarios[i])!=EOF)
             i++;
         fclose(pDireccion_Hora);
     }
-    //Fichero de informacion del mercado
 
-    pInfo=fopen("Informacion.txt", "r");
-    if(pInfo==NULL)
+    //Fichero de informacion acerca del mercado
+
+    pInformacion=fopen("Informacion.txt", "r");
+    if(pInformacion==NULL)
     {
         printf("Error al abrir fichero\n");
         return -1;
     }
     else
     {
-        fseek(pInfo, 0, SEEK_END);
-        SizeFicheroInfo=ftell(pInfo);
-        fseek(pInfo, 0, SEEK_SET);
-        Info=malloc(SizeFicheroDyH);
-        if(Info==NULL)
+        fseek(pInformacion, 0, SEEK_END);
+        SizeFicheroInformacion=ftell(pInformacion);
+        fseek(pInformacion, 0, SEEK_SET);
+        Informacion=malloc(SizeFicheroInformacion);
+        if(Informacion==NULL)
         {
             printf("Error: memoria insuficiente\n");
             exit(-1);
         }
         i=0;
-        while(fscanf(pInfo, "%c", &Info[i])!=EOF)
+        while(fscanf(pInformacion, "%c", &Informacion[i])!=EOF)
             i++;
-        fclose(pInfo);
+        fclose(pInformacion);
+    }
+
+    //Pantalla inicial
+
+    menu_inicio();
+    scanf("%c", &letratiendas_Acerca_Dir); //introducimos la letra correspondiente segun la opcion que queramos
+    switch(letratiendas_Acerca_Dir)
+    {
+    case 't':
+    case 'T':
+        tiendas();
+        break;
+    case 'a':
+    case 'A':
+        printf("%s\t", Informacion);
+        break;
+    case 'd':
+    case 'D':
+        printf("%s\t", DireccionyHorarios);
+        do
+        {
+            scanf("%c", &letrainicio);
+        }
+        while(letrainicio!='i'||letrainicio!='I');
+        menu_inicio();
+        break;
+
     }
     return 0;
 }
 void menu_inicio(void)
 {
-    printf("                   MERCADO  MALASANA                    \n\n\n");
+    printf("                         MERCADO  MALASANA                    \n\n\n");
     printf("||Tiendas(T)||\t ||Acerca del mercado(A)||\t ||Direccion y horarios(D)||\t\n\n");
 }
 void tiendas(void)
